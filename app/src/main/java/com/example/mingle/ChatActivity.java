f5d56d9aa83aa9b2e94bf513650988236f9ff46a
Sandra Ndirangu <Sandrandirangu40@gmail.com>
2024-07-30 22:33:35 +0300

    STT functionality completed

--- app/src/main/java/com/example/mingle/ChatActivity.java
+++ app/src/main/java/com/example/mingle/ChatActivity.java
@@ -1,17 +1,25 @@
 package com.example.mingle;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.os.Handler;
+import android.annotation.SuppressLint;
 import android.net.Uri;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+import android.widget.Toast;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.example.mingle.adapter.ChatRecyclerAdapter;
 import com.example.mingle.model.ChatMessageModel;
 import com.example.mingle.model.ChatroomModel;
@@ -24,12 +32,10 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.Query;
-
 import org.json.JSONObject;
-
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
-
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.MediaType;
@@ -53,14 +59,23 @@ public class ChatActivity extends AppCompatActivity {
     ImageView imageView;
 
 
+    private final Handler handler = new Handler();
+    int clickCount = 0;
+    long startTime;
+    long duration;
+    static final int MAX_DURATION = 500; // Maximum duration between taps for it to be considered a double tap
+    private static final long COUNTDOWN_INTERVAL = 100; // Adjusted for better timing
+    protected static final int RESULT_SPEECH = 1;
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
-        //get UserModel
+        // Get UserModel
         otherUser = AndroidUtil.getUserModelFromIntent(getIntent());
-        chatroomId = FirebaseUtil.getChatroomId(FirebaseUtil.currentUserId(),otherUser.getUserId());
+        chatroomId = FirebaseUtil.getChatroomId(FirebaseUtil.currentUserId(), otherUser.getUserId());
 
         messageInput = findViewById(R.id.chat_message_input);
         sendMessageBtn = findViewById(R.id.message_send_btn);
@@ -71,36 +86,124 @@ public class ChatActivity extends AppCompatActivity {
 
         FirebaseUtil.getOtherProfilePicStorageRef(otherUser.getUserId()).getDownloadUrl()
                 .addOnCompleteListener(t -> {
-                    if(t.isSuccessful()){
-                        Uri uri  = t.getResult();
-                        AndroidUtil.setProfilePic(this,uri,imageView);
+                    if (t.isSuccessful()) {
+                        Uri uri = t.getResult();
+                        AndroidUtil.setProfilePic(this, uri, imageView);
                     }
                 });
 
-        backBtn.setOnClickListener((v)->{
-            onBackPressed();
-        });
+        backBtn.setOnClickListener(v -> onBackPressed());
         otherUsername.setText(otherUser.getUsername());
 
-        sendMessageBtn.setOnClickListener((v -> {
+        sendMessageBtn.setOnClickListener(v -> {
             String message = messageInput.getText().toString().trim();
-            if(message.isEmpty())
-                return;
+            if (message.isEmpty()) return;
             sendMessageToUser(message);
-        }));
+        });
+
+        recyclerView.setOnTouchListener(new View.OnTouchListener() {
+            private boolean isMessageSent = false;
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return handleRecyclerViewTouch(event);
+            }
+
+            private boolean handleRecyclerViewTouch(MotionEvent event) {
+                int eventAction = event.getAction();
+                switch (eventAction) {
+                    case MotionEvent.ACTION_DOWN:
+                        startTime = System.currentTimeMillis();
+                        clickCount++;
+                        startCountdown();
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        stopCountdown();
+                        long time = System.currentTimeMillis() - startTime;
+                        duration += time;
+                        if (clickCount >= 2) {
+                            if (duration >= MAX_DURATION) {
+                                duration = 0;
+                            }
+
+                            //Logic for Activating Speech to Text
+                            Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "en-US");
+                            try {
+                                startActivityForResult(intent, RESULT_SPEECH);
+                                messageInput.setText("");
+                            } catch (ActivityNotFoundException e) {
+                                Toast.makeText(getApplicationContext(), "Your device doesn't support Speech to Text", Toast.LENGTH_SHORT).show();
+                                e.printStackTrace();
+                            }
+
+                            clickCount = 0;
+                            duration = 0;
+                            isMessageSent = false;
+                        }
+                        break;
+                }
+                return true;
+            }
+
+            private final Runnable countdownRunnable = new Runnable() {
+                @Override
+                public void run() {
+                    long elapsedTime = System.currentTimeMillis() - startTime;
+                    if (elapsedTime >= MAX_DURATION) {
+                        handler.removeCallbacks(this);
+                        String message = messageInput.getText().toString().trim();
+                        if (!message.isEmpty() && !isMessageSent) {
+                            sendMessageToUser(message);
+                            isMessageSent = true;
+                        }
+                    } else {
+                        handler.postDelayed(this, COUNTDOWN_INTERVAL);
+                    }
+                }
+            };
+
+            private void startCountdown() {
+                handler.post(countdownRunnable);
+            }
+
+            private void stopCountdown() {
+                handler.removeCallbacks(countdownRunnable);
+            }
+        });
 
         getOrCreateChatroomModel();
         setupChatRecyclerView();
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case RESULT_SPEECH:
+                if(resultCode == RESULT_OK && data != null){
+                    ArrayList<String> text = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    assert text != null;
+                    messageInput.setText(text.get(0));
+                }
+                break;
+        }
+    }
 
-    void setupChatRecyclerView(){
+    @Override
+    public void onBackPressed() {
+        Log.d("ChatActivity", "onBackPressed called");
+        super.onBackPressed();
+    }
+
+    void setupChatRecyclerView() {
         Query query = FirebaseUtil.getChatroomMessageReference(chatroomId)
                 .orderBy("timestamp", Query.Direction.DESCENDING);
 
         FirestoreRecyclerOptions<ChatMessageModel> options = new FirestoreRecyclerOptions.Builder<ChatMessageModel>()
-                .setQuery(query,ChatMessageModel.class).build();
+                .setQuery(query, ChatMessageModel.class).build();
 
-        adapter = new ChatRecyclerAdapter(options,getApplicationContext());
+        adapter = new ChatRecyclerAdapter(options, getApplicationContext());
         LinearLayoutManager manager = new LinearLayoutManager(this);
         manager.setReverseLayout(true);
         recyclerView.setLayoutManager(manager);
@@ -115,19 +218,18 @@ public class ChatActivity extends AppCompatActivity {
         });
     }
 
-    void sendMessageToUser(String message){
-
+    void sendMessageToUser(String message) {
         chatroomModel.setLastMessageTimestamp(Timestamp.now());
         chatroomModel.setLastMessageSenderId(FirebaseUtil.currentUserId());
         chatroomModel.setLastMessage(message);
         FirebaseUtil.getChatroomReference(chatroomId).set(chatroomModel);
 
-        ChatMessageModel chatMessageModel = new ChatMessageModel(message,FirebaseUtil.currentUserId(),Timestamp.now());
+        ChatMessageModel chatMessageModel = new ChatMessageModel(message, FirebaseUtil.currentUserId(), Timestamp.now());
         FirebaseUtil.getChatroomMessageReference(chatroomId).add(chatMessageModel)
                 .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
                     @Override
                     public void onComplete(@NonNull Task<DocumentReference> task) {
-                        if(task.isSuccessful()){
+                        if (task.isSuccessful()) {
                             messageInput.setText("");
                             sendNotification(message);
                         }
@@ -135,15 +237,15 @@ public class ChatActivity extends AppCompatActivity {
                 });
     }
 
-    void getOrCreateChatroomModel(){
+    void getOrCreateChatroomModel() {
         FirebaseUtil.getChatroomReference(chatroomId).get().addOnCompleteListener(task -> {
-            if(task.isSuccessful()){
+            if (task.isSuccessful()) {
                 chatroomModel = task.getResult().toObject(ChatroomModel.class);
-                if(chatroomModel==null){
-                    //first time chat
+                if (chatroomModel == null) {
+                    // First time chat
                     chatroomModel = new ChatroomModel(
                             chatroomId,
-                            Arrays.asList(FirebaseUtil.currentUserId(),otherUser.getUserId()),
+                            Arrays.asList(FirebaseUtil.currentUserId(), otherUser.getUserId()),
                             Timestamp.now(),
                             ""
                     );
@@ -153,77 +255,48 @@ public class ChatActivity extends AppCompatActivity {
         });
     }
 
-    void sendNotification(String message){
-
-       FirebaseUtil.currentUserDetails().get().addOnCompleteListener(task -> {
-           if(task.isSuccessful()){
-               UserModel currentUser = task.getResult().toObject(UserModel.class);
-               try{
-                   JSONObject jsonObject  = new JSONObject();
-
-                   JSONObject notificationObj = new JSONObject();
-                   notificationObj.put("title",currentUser.getUsername());
-                   notificationObj.put("body",message);
-
-                   JSONObject dataObj = new JSONObject();
-                   dataObj.put("userId",currentUser.getUserId());
-
-                   jsonObject.put("notification",notificationObj);
-                   jsonObject.put("data",dataObj);
-                   jsonObject.put("to",otherUser.getFcmToken());
-
-                   callApi(jsonObject);
-
-
-               }catch (Exception e){
-
-               }
-
-           }
-       });
-
+    void sendNotification(String message) {
+        FirebaseUtil.currentUserDetails().get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                UserModel currentUser = task.getResult().toObject(UserModel.class);
+                try {
+                    JSONObject jsonObject = new JSONObject();
+                    JSONObject notificationObj = new JSONObject();
+                    notificationObj.put("title", currentUser.getUsername());
+                    notificationObj.put("body", message);
+                    JSONObject dataObj = new JSONObject();
+                    dataObj.put("userId", currentUser.getUserId());
+                    jsonObject.put("notification", notificationObj);
+                    jsonObject.put("data", dataObj);
+                    jsonObject.put("to", otherUser.getFcmToken());
+                    callApi(jsonObject);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
     }
 
-    void callApi(JSONObject jsonObject){
-         MediaType JSON = MediaType.get("application/json; charset=utf-8");
-         OkHttpClient client = new OkHttpClient();
-        String url = "//fcm.googleapis.com/v1/projects/myproject-b5ae1/messages:send";
-        RequestBody body = RequestBody.create(jsonObject.toString(),JSON);
+    void callApi(JSONObject jsonObject) {
+        MediaType JSON = MediaType.get("application/json; charset=utf-8");
+        OkHttpClient client = new OkHttpClient();
+        String url = "https://fcm.googleapis.com/v1/projects/backend-7270b/messages:send";
+        RequestBody body = RequestBody.create(jsonObject.toString(), JSON);
         Request request = new Request.Builder()
                 .url(url)
                 .post(body)
-                .header("Authorization","Bearer YOUR_API_KEY")
+                .header("Authorization", "Bearer YOUR_API_KEY")
                 .build();
         client.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(@NonNull Call call, @NonNull IOException e) {
-
+                // Handle failure
             }
 
             @Override
             public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-
+                // Handle success
             }
         });
-
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
